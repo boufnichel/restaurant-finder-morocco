0cb141b936a2397f6db1f6e523e149f900a7657e
Boufnichel mohamed <boufnichel.mohamed@gmail.com>
2025-01-03 15:09:52 +0100

    Add missing core files for Vite startup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { RestaurantListing } from './components/RestaurantListing'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50 py-8">
+      <RestaurantListing />
+    </div>
+  )
+}
+
+export default App
