3602ea92e39603f17214c471776d172733493343
Boufnichel mohamed <boufnichel.mohamed@gmail.com>
2025-01-03 16:42:32 +0100

    Update Tailwind config to include rewind-ui styles

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+    './node_modules/@rewind-ui/core/dist/theme/styles.js',
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {},
+  },
+  plugins: [require("tailwindcss-animate")],
+}
